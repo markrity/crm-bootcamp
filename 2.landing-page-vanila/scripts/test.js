1482b27607cd7fd08aa54291ba8ad011d9c2fdc0
אתי רזניקוב <eti@h-MacBook-Pro-sl-ty.local>
2021-06-09 18:31:43 +0300

    landing-page-css.2

--- 2.landing-page-vanila/scripts/test.js
+++ 2.landing-page-vanila/scripts/test.js
@@ -4,3 +4,14 @@ var testfunction = function() {
     selector.style.backgroundColor = '#'+Math.random().toString(16).substr(-6);
 
 }
+let numberRegExp= new RegExp('^[0-9]+$');
+var phoneValidation= function(){
+    var phoneNumber = document.getElementById('phone').value;
+    if (numberRegExp.test(phoneNumber) === true){
+        document.getElementById("Text1").value = "";
+    }
+    else {
+            document.getElementById("Text1").value = "Oops ";
+    }
+ 
+}
