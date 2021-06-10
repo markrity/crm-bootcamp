86b1806b40786c057016795e585f4fd63b5c860f
Yuval Cohen <yuvalcohen@h-MacBook-Pro-sl-Yuval.local>
2021-06-10 15:05:43 +0300

    validation with errors

--- 2.landing-page-vanila/scripts/landing-page.js
+++ 2.landing-page-vanila/scripts/landing-page.js
@@ -1,32 +1,30 @@
 function validation(){
+
     var name = document.getElementById('name').value;
-    var isValid = validateName(name);
-    if (!isValid){
-        alert("name: " + isValid );
+    var error = document.getElementById('error-name');
+    if (!validateName(name)){
+        error.textContent = "Please enter valid name";
+        errorMsg(error);
         return false;
     }
-
 
     var mail = document.getElementById('mail').value;
-    var mailElement = document.getElementById('mail');
-    mailElement.addEventListener("input", function(event){
-        mailElement.setCustomValidity("I am expecting an e-mail address!");
-    })
-    var isValid = validateMail(mail);
-    if (!isValid){
-        alert("mail: " + isValid );
+    var error = document.getElementById('error-mail');
+    if (!validateMail(mail)){
+        error.textContent = "Please enter valid mail";
+        errorMsg(error);
         return false;
     }
 
-    
+    var error = document.getElementById('error-phone');
     var phone = document.getElementById('phone').value;
-    var isValid = validatePhone(phone);
-    if (!isValid){
-        alert("phone: " + isValid );
+    if (!validatePhone(phone)){
+        error.textContent = "Please enter valid phone";
+        errorMsg(error);
         return false;
     }
 
-
+    return true;
 }
 
 function validateName(name){
@@ -42,4 +40,24 @@ function validatePhone(phone){
 function validateMail(mail){
     var regex = /^[0-9a-zA-Z-_.]{1,30}@[a-zA-Z]{2,30}[.]{1}[a-zA-Z]{2,30}([.]{1}[a-zA-Z]{2,30}){0,1}$/;
     return regex.test(mail);
+}
+
+
+function cancelError(element){
+    var elementName = element.name;
+    var error;
+    if(elementName == 'name'){
+        error  = document.getElementById('error-name');
+    } else if (elementName == 'mail'){
+        error  = document.getElementById('error-mail');
+    } else if (elementName == 'phone'){
+        error  = document.getElementById('error-phone');
+    }
+    error.style.display = 'none';
+}
+
+function errorMsg(error){
+    error.style.display = 'inline';
+    error.style.color = "red";
+    return error;
 }
