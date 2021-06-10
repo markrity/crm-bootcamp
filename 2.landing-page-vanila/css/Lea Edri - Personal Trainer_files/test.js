782755a45afc0a2d62395ab880749471e932748b
אתי רזניקוב <eti@h-MacBook-Pro-sl-ty.local>
2021-06-10 14:04:42 +0300

    new-css

--- 2.landing-page-vanila/css/Lea Edri - Personal Trainer_files/test.js
+++ 2.landing-page-vanila/css/Lea Edri - Personal Trainer_files/test.js
@@ -1,100 +1,81 @@
-const fieldStatus = {
-    name: true,
+
+const fieldStatus= { 
+    name: true, 
     phone: true,
     email: true
 }
-
-/**
- * 
- * @param {*} e 
- * check while typing if the name doesn't contains invalid chars.
- */
 
 function nameValidation(e) {
     const name = document.getElementById("name").value;
     const isValidName = /^[a-zA-Z\s]*$/.test(name);
     if (isValidName) {
         document.getElementById("name-error").innerHTML = "";
-        fieldStatus.name = true;
-    } else {
+        fieldStatus.name= true;
+    }
+    else {
         document.getElementById("name-error").innerHTML = "Oops! Your name can only contain letters and spaces";
-        fieldStatus.name = false;
+        fieldStatus.name= false;
     }
 }
-/**
- * 
- * 
- * check that the name is at least 2 chars.
- */
 
-function nameLengthValidation() {
+function nameLengthValidation(){
     const name = document.getElementById("name").value;
-    if (name.length < 2) {
-        fieldStatus.name = false;
+    if (name.length<2){
+        fieldStatus.name= false;
         document.getElementById("name-error").innerHTML = "Oops,Your name must contain at least 2 letters";
     }
 }
-/**
- * 
- * @param {*} e 
- * check while typing if the phone doesn't contains invalid chars.
- */
+
 function phoneValidation(e) {
     const phoneNumber = document.getElementById("phone").value;
     const isOnlyDigit = /^\d+$/.test(phoneNumber);
     if (isOnlyDigit) {
-        fieldStatus.phone = true;
+        fieldStatus.phone= true;
         document.getElementById("phone-error").innerHTML = "";
-    } else {
-        fieldStatus.phone = false;
-        document.getElementById("phone-error").innerHTML = "Oops! A phone number should contain only numbers";
     }
+    else {
+        fieldStatus.phone= false;
+        document.getElementById("phone-error").innerHTML = "Oops! A phone number should contain only numbers";
+    }  
 }
 
-/**
- * 
- * 
- * check that the name is at least 2 chars.
- */
-
-function phoneLengthValidation() {
+function phoneLengthValidation(){
     const phone = document.getElementById("phone").value;
-    if (phone.length != 7) {
-        fieldStatus.phone = false;
+    if (phone.length!=7){
+        fieldStatus.phone= false;
         document.getElementById("phone-error").innerHTML = "Oops! A phone number should exactly 7 digits";
     }
 }
-/**
- * check that the email is valid.
- */
+
 
 function emailValidation() {
     const email = document.getElementById("email").value;
     const validEmail = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(email);
-    if (validEmail) {
-        fieldStatus.email = true;
+    if(validEmail){
+        fieldStatus.email= true;
         document.getElementById("email-error").innerHTML = "";
-    } else if (email.length == 0) {
-        fieldStatus.email = false;
+    }
+    else if (email.length==0){
+        fieldStatus.email= false;
         document.getElementById("email-error").innerHTML = "Oops! Email address is required";
-    } else if (!validEmail) {
-        fieldStatus.email = false;
+    }
+    else if (!validEmail){
+        fieldStatus.email= false;
         document.getElementById("email-error").innerHTML = "Oops! Invalid email address";
     }
-
+ 
 
 }
 
-/**
- * check that the all form fill is valid, when the user submit it
- */
+document.getElementById("form").addEventListener("submit", formValidation);
 
 function formValidation(e) {
     const form = document.forms.form;
     nameLengthValidation();
     phoneLengthValidation()
     emailValidation();
-    if (Object.keys(fieldStatus).some(key => !fieldStatus[key])) {
-        e.preventDefault();
-    }
+   if (Object.keys(fieldStatus).some(key => !fieldStatus[key])) {
+    e.preventDefault();
+   }
 }
+
