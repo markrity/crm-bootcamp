7860eab49fe34d3e106136dcf237d961630575d5
Eti Reznikov <eti@h-MacBook-Pro-sl-ty.local>
2021-06-15 17:10:57 +0300

    landing-page-express

--- 3.landing-page-node/index.js
+++ 3.landing-page-node/index.js
@@ -6,14 +6,53 @@ app.engine('html', mustacheExpress());
 
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
+app.use(express.static('views'));
+
+
 
 app.get('/', function(req, res) {
-  const data = {
-    a: 'hello world'
-  };
-  res.render('test', data);
+    const data = {
+        "title": "Lea Edri - Personal Trainer",
+        "area-codes": [
+            { "area-code": "050" },
+            { "area-code": "052" },
+            { "area-code": "053" },
+            { "area-code": "054" },
+            { "area-code": "055" },
+            { "area-code": "056" },
+            { "area-code": "058" },
+            { "area-code": "059" }
+        ],
+        "genders": [{
+                "gender": "male"
+            },
+            { "gender": "female" }
+        ],
+
+        "slogan1": "Don't wish for results -",
+        "slogan2": "work for it!",
+        "updates-slogan": "Get updates and special offers - promise not to spam you!",
+        "Name": "Name",
+        "phone-number": "Phone Number",
+        "email": "Email Address",
+        "more-info": "Something that you want us to know",
+        "btn-txt": "Submit"
+    };
+
+    res.render('index_.html', data);
+});
+
+app.get('/submitted', function(req, res) {
+    console.log(req.path)
+    console.log(req.query.name);
+    const data = {
+        "title": "Lea Edri - Personal Trainer",
+        "name": req.query.name,
+    };
+
+    res.render('ThankYou', data);
 });
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server running at http://localhost:${process.env.PORT}/`);
+    console.log(`Server running at http://localhost:${process.env.PORT}/`);
 });
