d686ac852843710bf64678e4a422999a4b1729bd
Nadav Yihie <nadavyihye@gmail.com>
2021-06-15 14:08:12 +0300

    15.6

--- 3.landing-page-node/index.js
+++ 3.landing-page-node/index.js
@@ -1,19 +1,59 @@
 const express = require('express');
 const app = express();
 const mustacheExpress = require('mustache-express');
-
 app.engine('html', mustacheExpress());
-
+app.use(express.static('views'));
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
+
+
 app.get('/', function(req, res) {
-  const data = {
-    a: 'hello world'
-  };
-  res.render('test', data);
+
+
+  const data={ img:'css/background.jpg',
+                time: returnTime()              
+          };
+  
+
+  console.log(data.time);
+  const q=req.query;
+  
+  res.render('landPage',data);
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running at http://localhost:${process.env.PORT}/`);
+
+app.get('/tankYouPage', function(req, res) {
+
+
+  const data={ img:'css/background.jpg',
+                time: returnTime()              
+          };
+  
+
+  console.log(data.time);
+  const q=req.query;
+  
+  res.render('landPage',data);
 });
+
+app.listen( '8080', () => {
+  console.log(`Server running at http://localhost:8080/`);
+});
+
+function returnTime(){
+  let currentTime=new Date();
+  let timeString="";
+  let currentHour=currentTime.getHours();
+  if(currentHour>5&&currentHour<12){
+    return 'morning';
+  }
+  if(currentHour>12&&currentHour<18){
+    return 'afternoon';
+  }
+  if(currentHour>18&&currentHour<22){
+    return 'evening';
+  }
+  return 'night';
+
+}
