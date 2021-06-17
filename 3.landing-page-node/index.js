d10305998f2c4349c6934921459af1d841d3bbbe
Neta Carmiel <netacarmiel@h-MBP-sl-Neta.Dlink>
2021-06-17 21:50:12 +0300

    17.6

--- 3.landing-page-node/index.js
+++ 3.landing-page-node/index.js
@@ -1,18 +1,70 @@
 const express = require('express');
-const app = express();
+// const helpers = require('./helpers'); //Requires the help functions.
 const mustacheExpress = require('mustache-express');
+const axios = require('axios');
+const app = express();
+app.use(express.static('views'));
 
 app.engine('html', mustacheExpress());
-
 app.set('view engine', 'html');
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/views'); //For calling from location down.
+
+
+app.get('/admin', function(req, res) {
+  // console.log('bla')
+  axios.get('http://kerenadiv.com:8004/admin').then((response) => {
+    // console.log('here');
+    var leads = response.data;
+    var data = {
+      
+      leads,
+      leads_json:JSON.stringify(leads)
+    
+    }
+    console.log(leads)
+    console.log(data)
+    res.render('leadsTable',data);
+   // res.send(leads);
+  });  
+});
+
+
+
+
+app.get('/:id', function(req, res) {
+  var picId=req.params.id;
+  
+  const data = {
+    "image": [
+      {src: "Images/pic1.jpg", value:1, class:""},
+      {src: "Images/pic2.jpg", value:2, class:""},
+      {src: "Images/pic3.jpg", value:3, class:""},
+      {src: "Images/pic4.jpg", value:4, class:""},
+      {src: "Images/pic5.jpg", value:5, class:""},
+      {src: "Images/pic6.jpg", value:6, class:""}
+    ]
+  }
+  
+  data["image"][picId-1].class="visible";
+  res.render('landingPage',data);
+
+});
+
 
 app.get('/', function(req, res) {
   const data = {
-    a: 'hello world'
-  };
-  res.render('test', data);
+    "image": [
+      {src: "Images/pic1.jpg", value:1, class:"visible"},
+      {src: "Images/pic2.jpg", value:2, class:""},
+      {src: "Images/pic3.jpg", value:3, class:""},
+      {src: "Images/pic4.jpg", value:4, class:""},
+      {src: "Images/pic5.jpg", value:5, class:""},
+      {src: "Images/pic6.jpg", value:6, class:""}
+    ]
+  }
+  res.render('landingPage',data);
 });
+
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running at http://localhost:${process.env.PORT}/`);
--- 4.leads-api/index.js
+++ 4.leads-api/index.js
@@ -1,10 +1,77 @@
+var mysql = require('mysql');
+  
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "Project"
+});
+
+
 const express = require('express');
 const app = express();
 
-app.get('/', function(req, res) {
-  res.send('hello there');
+var cors = require('cors')
+app.use(cors());
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/admin', function(req, res) {
+  // con.connect(function(err) {
+    con.query("SELECT * FROM leads", function (err, result, fields) {
+      if (err) throw err;
+      //console.log(result);
+      res.send(result);
+    });
+
+  });
+// });
+
+app.post('/', function(req, res) {
+  var { firstname,lastname,phone} = req.body;
+  console.log(firstname);
+  console.log(req.body);
+  var invalid_type="";
+
+  invalid_type = validateName(firstname,"first")
+  invalid_type+= validateName(lastname,"last")
+  invalid_type+= validatePhone(phone)
+    
+  if (invalid_type==="") {
+      console.log("Connected!");
+      var sql = `INSERT INTO leads (first_name, last_name, phone) VALUES ('${firstname}', '${lastname}','${phone}')`;
+      con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("1 record inserted");
+      });
+    
+    res.json({status:true})
+  }
+  else {
+    res.json({status:false, reason:invalid_type})
+  }
 });
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running at http://localhost:${process.env.PORT}/`);
 });
+
+function validatePhone(phone,invalid_type) {  
+  var phoneRGEX = /^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/;
+  var phoneResult = phoneRGEX.test(phone);
+  if (!phoneResult) {
+    return "invalid phone \r\n";
+  }
+  return "";
+}
+
+function validateName(name,index) {  
+  var NameRGEX = /^[a-zA-Z ]+$/ ;
+  var nameResult = NameRGEX.test(name);
+  if (!nameResult) {
+    return "invalid " + index + " name \r\n";
+  }
+  return "";
+}
