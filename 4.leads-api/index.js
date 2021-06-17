9939563ae710426d561e6b57b94e1de67c2a66d7
Yuval Cohen <yuvalcohen@h-MacBook-Pro-sl-Yuval.local>
2021-06-17 15:34:07 +0300

    leads api- done

--- 4.leads-api/index.js
+++ 4.leads-api/index.js
@@ -4,11 +4,16 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.use(express.urlencoded({extended: true}));
+// app.use(express.json())
+
 app.use(cors());
 app.use(express.urlencoded( {extended: true} ));
 
 // connecting to the db
 var mysql = require('mysql');
+const { query } = require('express');
 var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -18,6 +23,15 @@ var connection = mysql.createConnection({
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running at http://localhost:${process.env.PORT}/`);
+});
+
+var connectionValid = false;
+connection.connect(function(err) {
+  if (err){
+    connectionValid = false;
+  } else {
+    connectionValid = true;
+  }
 });
 
 
@@ -41,24 +55,21 @@ app.post('/', function(req, res) {
     resData.errors.push("error-phone");
   }
   
-  // trying to save the lead to the db
-  connection.connect(function(err) {
-    if (err){
-      resData.valid = false;
-      resData.errors.push( "error-server");
-    } else {
-      console.log("Connected!");
-      var sql = `INSERT INTO leads (user_name, user_mail, user_phone) VALUES ('${body.fullName}', '${body.email}', ${body.phone});`;
-      connection.query(sql, function (err, result) {
-        if (err){
-          resData.valid = false;
-          resData.errors.push( "error-server");
-        } else {
-          console.log("1 record inserted");
-        }
-      });
-    }
-  });
+  if(!connectionValid){
+    resData.valid = false;
+    resData.errors.push( "error-server");
+  } else {
+    // trying to save the lead to the db
+    var sql = `INSERT INTO leads (user_name, user_mail, user_phone) VALUES ('${body.fullName}', '${body.email}', ${body.phone});`;
+    connection.query(sql, function (err, result) {
+      if (err){
+        resData.valid = false;
+        resData.errors.push( "error-server");
+      } else {
+        console.log("1 record inserted");
+      }
+    })
+  }
 
   res.send(resData);
 });
@@ -89,3 +100,35 @@ function validateMail(mail){
   var regex = /^[0-9a-zA-Z-_.]{1,30}@[a-zA-Z]{2,30}[.]{1}[a-zA-Z]{2,30}([.]{1}[a-zA-Z]{2,30}){0,1}$/;
   return regex.test(mail);
 }
+
+
+var orderMap = {"ascending": "ASC", "descending": "DESC"};
+app.get('/leads', function(req, res) {
+  const order = req.query.order;
+  console.log(req);
+  var resData = {};
+  var orderBy = "";
+  if(!connectionValid){
+    resData.valid = false;
+    resData.errors.push( "error-server");
+  } else {
+      console.log("Connected!");
+      if(order) {
+          orderBy = " ORDER BY user_name " + orderMap[order];
+      }
+      var sql = 'SELECT user_name, user_mail, user_phone FROM leads' + orderBy +";";
+      console.log(sql);
+      connection.query(sql, function (err, results) {
+        if (err){
+          console.log("error :( !!!!!!!!");
+          res.send("error");
+        } else {
+          console.log(orderMap[order]);
+          console.log(results);
+          resData.leads = results;
+          res.send(resData);
+        }
+      });
+  }
+
+});
